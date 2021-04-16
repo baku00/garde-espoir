472f605776c365c71098742bf4018c3650f83bae
unknown <david.gloriod@rpn.ch>
2021-04-16 05:33:37 +0200

    maxlength 255 => 300

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -21,7 +21,7 @@
       @csrf
       <input type="file" id="picture" class="form-control" name="picture" value="{{old('picture')}}" placeholder="Picture">
       <input type="text" id="title" class="form-control" name="title" value="{{old('title')}}" placeholder="Title (20 caractères max)" maxlength="20">
-      <input type="text" id="description" class="form-control" name="description" value="{{old('description')}}" placeholder="Description (255 caractères max)" maxlength="255">
+      <input type="text" id="description" class="form-control" name="description" value="{{old('description')}}" placeholder="Description (300 caractères max)" maxlength="300">
       <button type="button" name="button" class="btn btn-primary" onclick="create()">Ajouter</button>
     </form>
   </div>
